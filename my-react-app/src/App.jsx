1e193de8159fd95ccb45487951de156d75eb38b5
ketsaraaw <muhammadketsar45@gmail.com>
2023-11-01 16:21:05 +0700

    [FEATURE] first setup project

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex justify-center min-h-screen items-center">
+      <h1 className="">Ketsar Ali</h1>
+    </div>
+  )
+}
+
+export default App
