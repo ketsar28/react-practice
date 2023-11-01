cef508f6f940d9cb3d90756e4736545ad3ad9ea2
ketsaraaw <muhammadketsar45@gmail.com>
2023-11-01 21:31:55 +0700

    [FEATURE] fff

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,13 +1,32 @@
-import { InputForm } from "../Elements/Input/Index"
-import {Button} from "../Elements/Button/Button"
+import { InputForm } from "../Elements/Input/Index";
+import { Button } from "../Elements/Button/Button";
 
 export const FormLogin = () => {
-    return (
-        <form action="">
-        <InputForm label="Email" type="email" placeholder="email@gmail.com" name="email"/>
-        <InputForm label="Password" type="password" placeholder="****" name="password"/>
-        
-        <Button text="Sign In" variant="bg-green-600 w-full"/>
-      </form>
-    )
-}
+  const handleLogin = (e) => {
+    e.preventDefault();
+    localStorage.setItem('email', e.target.email.value);
+    localStorage.setItem('password', e.target.password.value);
+    window.location.href="/products";
+    // ! console.log(e.target.email.value);
+    // ! console.log(e.target.password.value);
+    console.log("login");
+  };
+  return (
+    <form onSubmit={handleLogin}>
+      <InputForm
+        label="Email"
+        type="email"
+        placeholder="email@gmail.com"
+        name="email"
+      />
+      <InputForm
+        label="Password"
+        type="password"
+        placeholder="****"
+        name="password"
+      />
+
+      <Button text="Sign In" variant="bg-green-600 w-full" type="submit" />
+    </form>
+  );
+};
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/jsx-key */
+import { Fragment } from "react";
 import { ProductCard } from "../components/Fragments/ProductCard";
+import { Button } from "../components/Elements/Button/Button";
 
 const products = [
   {
@@ -31,18 +33,31 @@ const products = [
   },
 ];
 
+const user = localStorage.getItem("email");
+
 export const ProductPage = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.href = "/login";
+  };
   return (
-    <div className="flex justify-center py-5 flex-wrap gap-5">
-      {products.map((product, id) => (
-        <ProductCard key={id}>
-          <ProductCard.Header image={product.image} alt={product.alt} />
-          <ProductCard.Body name={product.name}>
-            {product.description}
-          </ProductCard.Body>
-          <ProductCard.Footer price={product.price} />
-        </ProductCard>
-      ))}
-    </div>
+    <Fragment>
+      <div className="flex justify-end h-20 items-center bg-green-500 px-10 gap-5">
+        <span className="text-white font-bold">{user}</span>
+        <Button variant="bg-red-600" text="Sign Out" onClick={handleLogout} />
+      </div>
+      <div className="flex justify-center py-5 flex-wrap gap-5">
+        {products.map((product, key) => (
+          <ProductCard key={key}>
+            <ProductCard.Header image={product.image} alt={product.alt} />
+            <ProductCard.Body name={product.name}>
+              {product.description}
+            </ProductCard.Body>
+            <ProductCard.Footer price={product.price} />
+          </ProductCard>
+        ))}
+      </div>
+    </Fragment>
   );
 };
